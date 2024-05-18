6237a86de56bf93cb4b179ba9d828ac1de8526be
Elvanss <duy.elvans12@gmail.com>
2024-05-18 10:48:19 +1000

    lasted Update req7

--- src/main/java/com/smart/inventory/Service/ReportService.java
+++ src/main/java/com/smart/inventory/Service/ReportService.java
@@ -26,20 +26,38 @@ public class ReportService {
 
     public ReportDTO generateReport(Long profileId) {
         Profile profile = this.profileService.getProfileById(profileId);
-        List<Item> consumedItems = this.profileService.getItemsForProfile(profileId);
+        List<Item> itemRemaining = this.profileService.getItemsForProfile(profileId);
+
+        // Filter out consumption records that are not for the profile
+        profile.setConsumptionRecords(profile.getConsumptionRecords().stream()
+                .filter(cr -> cr.getProfile().getId().equals(profileId))
+                .toList());
 
         ReportDTO reportDTO = new ReportDTO();
         reportDTO.setProfileName(profile.getName());
         reportDTO.setProfileAge(profile.getAge());
         reportDTO.setProfileDietary(profile.getDietary());
 
-        reportDTO.setConsumedItems(consumedItems.stream()
+        reportDTO.setConsumedItems(itemRemaining.stream()
                 .map(Item::getName)
                 .collect(Collectors.toList()));
-        reportDTO.setTotalCaloriesConsumed(consumedItems.stream()
+
+        // Calculate total remaining calories, protein, and fat
+        reportDTO.setTotalCaloriesRemaining(itemRemaining.stream()
                 .mapToDouble(Item::getCalories).sum());
-        reportDTO.setTotalProteinConsumed(consumedItems.stream()
+        reportDTO.setTotalProteinRemaining(itemRemaining.stream()
                 .mapToDouble(Item::getProtein).sum());
+        reportDTO.setTotalFatRemaining(itemRemaining.stream()
+                .mapToDouble(Item::getFat).sum());
+
+        // Calculate total consumed calories, protein, and fat
+        reportDTO.setTotalCaloriesConsumed(profile.getConsumptionRecords().stream()
+                .mapToDouble(cr -> cr.getItem().getCalories()).sum());
+        reportDTO.setTotalProteinConsumed(profile.getConsumptionRecords().stream()
+                .mapToDouble(cr -> cr.getItem().getProtein()).sum());
+        reportDTO.setTotalFatConsumed(profile.getConsumptionRecords().stream()
+                .mapToDouble(cr -> cr.getItem().getFat()).sum());
+
 
         return reportDTO;
     }
@@ -50,6 +68,7 @@ public class ReportService {
         List<Profile> profiles = profileService.getAllProfilesForUser(user.getId());
         List<ReportDTO> reportDtos = new ArrayList<>();
 
+
         LocalDate currentDate = LocalDate.now();
         LocalDate lastWeekDate = currentDate.minusWeeks(1);
 
@@ -59,22 +78,38 @@ public class ReportService {
             reportDto.setProfileAge(profile.getAge());
             reportDto.setProfileDietary(profile.getDietary());
 
+            // Filter out consumption records that are not for the profile
             List<ConsumptionRecord> weeklyConsumptionRecords = profile.getConsumptionRecords().stream()
                     .filter(cr -> cr.getConsumedAt().toLocalDate().isAfter(lastWeekDate) &&
                             cr.getConsumedAt().toLocalDate().isBefore(currentDate))
                     .toList();
 
+            // Calculate total remaining calories, protein, and fat
+            reportDto.setTotalCaloriesRemaining(profile.getFridgeInventory().getItems().stream()
+                    .mapToDouble(Item::getCalories)
+                    .sum());
+            reportDto.setTotalProteinRemaining(profile.getFridgeInventory().getItems().stream()
+                    .mapToDouble(Item::getProtein)
+                    .sum());
+            reportDto.setTotalFatRemaining(profile.getFridgeInventory().getItems().stream()
+                    .mapToDouble(Item::getFat)
+                    .sum());
+
+            // Calculate total consumed items
             reportDto.setConsumedItems(weeklyConsumptionRecords.stream()
                     .map(cr -> cr.getItem().getName())
                     .distinct()
                     .toList());
 
+            // Calculate total consumed calories, protein, and fat
             reportDto.setTotalCaloriesConsumed(weeklyConsumptionRecords.stream()
                     .mapToDouble(cr -> cr.getItem().getCalories())
                     .sum());
-
             reportDto.setTotalProteinConsumed(weeklyConsumptionRecords.stream()
                     .mapToDouble(cr -> cr.getItem().getProtein())
+                    .sum());
+            reportDto.setTotalFatConsumed(weeklyConsumptionRecords.stream()
+                    .mapToDouble(cr -> cr.getItem().getFat())
                     .sum());
 
             reportDtos.add(reportDto);
